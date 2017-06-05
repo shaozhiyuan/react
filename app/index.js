7b278f18f3b0c9dd72508cdf9741568d65757120
shaozhiyuan <632068012@qq.com>
2017-06-05 10:14:22 +0800

    remove all

--- app/index.js
+++
@@ -1,8 +0,0 @@
-import _ from "lodash";
-import $ from "jquery";
-function component() {
-    var element = $("<div></div>");
-    element.html(_.join(['hello','webpack']," "));
-    return element.get(0)
-}
-document.body.appendChild(component());
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    entry: "./app/index.js",
-    output: {
-        filename: "bundle.js",
-        path: __dirname+"dist"
-    }
-}
